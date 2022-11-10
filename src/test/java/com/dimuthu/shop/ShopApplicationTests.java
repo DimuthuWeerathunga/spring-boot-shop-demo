00215a68f1301e718dd088c5de52d1e70e14b2f7
Dimuthu Weerathunga <dimu.wa.98@gmail.com>
2022-11-10 07:47:09 +0530

    initial commit

---
+++ src/test/java/com/dimuthu/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimuthu.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
